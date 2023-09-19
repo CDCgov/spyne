2a8c6c40377d248adaa14349c3984be4b2530d3d
KristineLacek <qgx6@cdc.gov>
2023-09-19 16:23:03 -0400

    include amended final bams in bam tarball

--- workflow/scripts/spyne_cleanup.sh
+++ workflow/scripts/spyne_cleanup.sh
@@ -4,7 +4,7 @@
 tar --remove-files -czf spyne_logs.tar.gz .snakemake logs IRMA/*/logs 
 
 # tar gzip archive plurality fasta, F1*bam/ref
-tar --remove-files -czf irma_allconsensus_bam.tar.gz IRMA/*/*fasta IRMA/*/intermediate/4-*/F1* IRMA/*/amended_consensus
+tar --remove-files -czf irma_allconsensus_bam.tar.gz IRMA/*/*fasta IRMA/*/intermediate/4-*/F1* IRMA/*/amended_consensus IRMA/*/*.bam
 
 # remove the rest of IRMAs output
 rm -fr IRMA
