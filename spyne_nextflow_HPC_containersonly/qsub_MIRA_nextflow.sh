dcc2cb0c616596b2b46caa2f573aa9873cd3ab6e
try8-cdc <121120396+try8-cdc@users.noreply.github.com>
2024-01-10 11:19:41 -0500

    Update qsub_MIRA_nextflow.sh

--- spyne_nextflow_HPC_containersonly/qsub_MIRA_nextflow.sh
+++ spyne_nextflow_HPC_containersonly/qsub_MIRA_nextflow.sh
@@ -16,4 +16,4 @@ bash MIRA_nextflow.sh \
 	-s /scicomp/home-pure/try8/nextflow/FLU_SC2_SEQUENCING_nextflow/tiny_test_run_flu_illumina/samplesheet.csv \
 	-r /scicomp/home-pure/try8/nextflow/FLU_SC2_SEQUENCING_nextflow/tiny_test_run_flu_illumina \
 	-e Flu_Illumina  \
-  -c True
+        -t
