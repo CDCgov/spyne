ea2e99f9eb5d1b2a49d55bb404f819d62e5a6f32
KristineLacek <qgx6@cdc.gov>
2024-01-26 12:20:08 -0500

    first go at csv/xlsx to parq python function

---
+++ scripts/parquet_maker.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import argparse
+from pathlib import Path
+import pyarrow
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file")
+parser.add_argument("-o", "--outputname")
+parser.add_argument("-r", "--runid")
+parser.add_argument("-i", "--instrument")
+
+inputarguments = parser.parse_args()
+
+if inputarguments.file:
+    infi = inputarguments.file
+else:
+    exit(0)
+
+if inputarguments.outputname:
+    outfi = inputarguments.outputname
+else:
+    exit(0)
+
+if inputarguments.runid:
+    run_id = inputarguments.runid
+else:
+    run_id = Path.cwd()
+    run_id = str(run_id).split('/')[-1]
+
+if inputarguments.instrument:
+    instrument = inputarguments.instrument
+else:
+    instrument = "testInstrument"
+
+
+if ".csv" in infi:
+    table = pd.read_csv(infi, header=0)
+elif ".xls" in infi:
+    table = pd.read_excel(infi, header=0)
+elif ".txt" in infi or ".tsv" in infi:
+    table = pd.read_csv(infi, sep="/t", header=0)
+
+table['runid'] = run_id
+
+table['instrument'] = instrument
+
+#remove the header
+pd.DataFrame.to_csv(table, "temp.csv", header=False, index=False)
+table = pd.read_csv("temp.csv", header=None)
+print(table)
+
+pd.DataFrame.to_parquet(table, outfi, index=False)
