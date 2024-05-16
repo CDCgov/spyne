de3d8d7d3c40bc64d9608dda833595872ff715f9
Amanda Sullivan <yxpa3@cdc.gov>
2024-05-16 15:20:01 -0400

    remove qsub script updated MIRA_nextflow.sh

--- spyne_nextflow_HPC_containersonly/MIRA_nextflow.sh
+++ spyne_nextflow_HPC_containersonly/MIRA_nextflow.sh
@@ -1,101 +1,31 @@
 #!/bin/bash
-# Wrapper to perform genome assembly
-
-source /etc/profile
-
-usage() {
-	echo -e "THIS IS AN EDIT! \n Usage in Spyne container: \n docker exec spyne bash $0 -s {path to samplesheet.csv starting with /data/} -r <run_id starting with /data> -e <experiment_type> <OPTIONAL: -p amplicon_library> <optional: -c CLEANUP-FOOTPRINT> \n
-Usage in git cloned CLI: \n bash $0 -s {path to samplesheet.csv } -r <run_id > -e <experiment_type> <OPTIONAL: -p amplicon_library> <optional: -c CLEANUP-FOOTPRINT> <optional: -n > <optional: -t >" 1>&2
-	exit 1
-}
+#$ -o $1/nextflow.out
+#$ -e $1/nextflow.err
+#$ -N Illumina_influenza_nextflow
+#$ -pe smp 2
+#$ -l h_rt=72:00:00
+#$ -l h_vmem=32G
+#$ -q flu.q
+#$ -cwd
+#$ -V
 
 # Experiment type options: Flu-ONT, SC2-Spike-Only-ONT, Flu_Illumina, SC2-Whole-Genome-ONT, SC2-Whole-Genome-Illumina
 # Primer Schema options: articv3, articv4, articv4.1, articv5.3.2, qiagen, swift, swift_211206
 
-while getopts 's:r:e:p:c:nat' OPTION; do
-	case "$OPTION" in
-	s) SAMPLESHEET="$OPTARG" ;;
-	r) RUNPATH="$OPTARG" ;;
-	e) EXPERIMENT_TYPE="$OPTARG" ;;
-	p) PRIMER_SCHEMA="$OPTARG" ;;
-	c) CLEANUP="$OPTARG" ;;
-	n) NOCONTAINER=True ;;
-	a) APPLICATION=True ;;
-	t) TAR=True ;;
-	*) usage ;;
-	esac
-done
+source /etc/profile
+RUNPATH='/scicomp/scratch/xpa3/FLU_SC2_SEQUENCING_nextflow/tiny_test_run_flu_illumina'
+TAR=True
 
-if [ -z "${SAMPLESHEET}" ] || [ -z "${RUNPATH}" ] || [ -z "${EXPERIMENT_TYPE}" ]; then
-	usage
+# Archive previous run using the summary.xlsx file sent in email
+if [ -f "./summary.xlsx" ] && [ -n "${TAR}" ]; then
+	tar --remove-files -czf ${RUNPATH}/previous_run_$(date -d @$(stat -c %Y ./summary.xlsx) "+%Y%b%d-%H%M%S").tar.gz ${RUNPATH}/*html ${RUNPATH}/*fasta ${RUNPATH}/*txt
 fi
-
-if [ -z "${PRIMER_SCHEMA}" ]; then
-	OPTIONALARGS=""
-else
-	OPTIONALARGS="-p $PRIMER_SCHEMA"
-fi
-
-if [ -z "${CLEANUP}" ]; then
-	OPTIONALARGS="${OPTIONALARGS}"
-else
-	OPTIONALARGS="${OPTIONALARGS} -c ${CLEANUP}"
-fi
-
-# Run whatever Bash commands here
-# Set paths for non-container dev work
-#if [ $NOCONTAINER ]; then
-SCRIPT=$(realpath -s "$0")
-echo "${SCRIPT}"
-RESOURCE_ROOT=$(dirname "$SCRIPT")
-echo "${RESOURCE_ROOT}"
-#	BBTOOLS_ROOT=~/bbmap
-JAVA_ROOT=$(which java) #; else #set paths in container
-#	RESOURCE_ROOT=/spyne
-#	BBTOOLS_ROOT=/opt/bbtools
-#	JAVA_ROOT=/opt/java
-#fi
-
-# Export bbtools to system path
-#bbtools_path=$(ls ${BBTOOLS_ROOT})
-
-#for eachdir in ${bbtools_path}
-#do
-#	export PATH=$PATH:${BBTOOLS_ROOT}/${eachdir}
-#done
-
-# Export java to system path
-java_path=$(ls ${JAVA_ROOT})
-
-for eachdir in ${java_path}; do
-	export PATH=$PATH:${JAVA_ROOT}/${eachdir}/bin
-done
-
-# Check the java version
-if [ -z "${NOCONTAINER}" ]; then
-	java -version
-fi
-# Check the docker version
-docker --version
-
-#run_path=$(dirname $(readlink -f $RUNPATH/))
-
-# Archive previous run
-if [ -f "${RUNPATH}/report*.html" ] && [ -n "${TAR}" ]; then
-	tar --remove-files -czf ${RUNPATH}/previous_run_$(date -d @$(stat -c %Y ${RUNPATH}/report.html) "+%Y%b%d-%H%M%S").tar.gz ${RUNPATH}/*html ${RUNPATH}/*fasta ${RUNPATH}/*txt
-fi
-
-#if [ $APPLICATION ]; then
-#	CLI=""; else
-#	CLI="-m"
-#fi
 
 # Run nextflow
-module load nextflow/23.04.02
-nextflow run "$RESOURCE_ROOT"/workflow/illumina_influenza_nextflow.nf \
-	--s "$SAMPLESHEET" \
+module load nextflow/23.10.0
+nextflow run ./workflow/illumina_influenza_nextflow.nf \
+	--s "$RUNPATH"/samplesheet.csv \
 	--r "$RUNPATH" \
-	--e "$EXPERIMENT_TYPE" \
-	-c "$RESOURCE_ROOT"/nextflow.config \
-	-profile singularity,rosalind \
-	$CLI $OPTIONALARGS
+	--e Flu_Illumina \
+	-c ./nextflow.config \
+	-profile singularity,rosalind
